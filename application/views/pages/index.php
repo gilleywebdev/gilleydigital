ecaa18ca37528e413261501d30b3d47306378bd5
Chris Gilley <chris@gilleywebdev.com>
2012-09-13 01:10:51 -0400

    Wednesday's edits

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,10 +1,21 @@
 <h1>Gilley Digital is an independent web shop based in Portland, Maine</h1>
-<div class="column">
-	<h3>Responsive Design</h3>
-	<p>Responsive Design means the site is made to stretch and be readable no matter the size of the window or device.</p>
+<div class="column_wrap">
+	<div class="column">
+		<h3 class="column_header">Responsive Design</h3>
+		<p>Responsive Design means the site is made to stretch and be readable no matter the size of the window or device.</p>
+		<h3 class="column_header">Search Engine Optimized</h3>
+		<p>SEO means that your site is engineered to appear near the top of the search results for certain searches related to your business, like "Best Fishmonger Portland, Maine".</p>
+	</div>
+	<div class="column">
+		<h3 class="column_header">Content Management</h3>
+		<p>A CMS is a system that allows you and your employees to log in and easily make text changes to the site, without needing to know any code.</p>
+		<h3 class="column_header">Sturdy Scaleable Framework</h3>
+		<p>I build all new websites in Kohana, an open source programming framework.</p>
+	</div>
+	<div class="cf"></div>
 </div>
-<div class="column">
-	<h3>Content Management</h3>
-	<p>A CMS is a system that allows you and your employees to log in and easily make text changes to the site, without needing to know any code.</p>
+<div class="testimonial">
+	<h2 style="margin:0">Chris made the process of creating a website easy and enjoyable. I would recommend him to anyone looking for a professional experience and a great product.</h2>
+	<p style="float: right">- Julianna Orgel, julesinthejungle.com</p>
+	<div class="cf"></div>
 </div>
-<div class="cf"></div>
